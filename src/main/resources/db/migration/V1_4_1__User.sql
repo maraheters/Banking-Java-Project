63959400fb6eb7cdd98f9b38ac3f6f63ba073112
maraheters <blr.vladdd@gmail.com>
2025-03-03 14:01:17 +0300

    refactor(database): add index on user.email

--- src/main/resources/db/migration/V1_4_1__User.sql
+++ src/main/resources/db/migration/V1_4_1__User.sql
@@ -3,4 +3,6 @@ CREATE TABLE public.user (
     name            VARCHAR(255)    NOT NULL,
     email           VARCHAR(255)    NOT NULL UNIQUE,
     password_hash   VARCHAR(255)    NOT NULL
-)
+);
+
+CREATE INDEX idx_user_email ON public.user(email);
